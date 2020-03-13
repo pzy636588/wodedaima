8e6b730efab391e6978fd03999fe7cd6e4587db8
pengziyu <13530696246@163.com>
2020-03-13 22:47:34 +0800

    元组

---
+++ 元组的创建和删除.py
@@ -0,0 +1,9 @@
+num=(2,45,78,8,56,23,5,159)
+sfjfj=("高山流水","渔舟唱晚")
+untitle=("pyhon",85,("人生苦短","我用python"),["爬虫","自动化运维","元计算"])
+
+verse=("酒逢知己千杯少",)
+print("verse的类型为：",type(verse))   #在python中可以用type()测试变量的类型
+
+tuple(range(2,58,78))
+#print(range)
