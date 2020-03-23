6578491806c6ce9471d56ca7636d43a52939649f
pengziyu <13530696246@163.com>
2020-03-23 22:50:22 +0800

    字典推导式。集合

--- 字典推导式.py
+++ 字典推导式.py
@@ -1,3 +1,10 @@
 import random
-randomdict={i:random.randint(2,800) for i in range (1,8)}
+randomdict={i:random.randint(2,5000) for i in range (1,8)}
 print('生成的字典为:',randomdict)
+
+
+
+name=['绮梦','姜子牙','诸葛亮']
+sign=['天蝎','射手','魔蝎']
+dictionary={i:j+'座' for i,j in zip(name,sign)}   #使用列表推导式生成字典
+print(dictionary)
---
+++ 集合.py
@@ -0,0 +1,7 @@
+#直接使用{}创建集合
+#setname={element1,element2,,...,elementn}
+#setname表示集合的名称，用python命名规则的标识符
+#element1,element2,,...,elementn表示集合中的元素，个数没有限制，只要是python支持的类型就可以
+set1={1,5,6,6,8,5,9,2,89}
+set2={'Python',25,('人生苦短','我用pyhon')}
+print(set2)
