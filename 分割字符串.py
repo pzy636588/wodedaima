8226bf85bbeb50e8c364fd7d2f2e71a4473c3292
pengziyu <13530696246@163.com>
2020-03-30 23:02:34 +0800

    自考复习，分割字符串

---
+++ 分割字符串.py
@@ -0,0 +1,13 @@
+str1='明 日 学 院 官 网 >>> www.mingrisoft.com'
+print('原字符串',str1)
+list1=str1.split()
+print(list1)
+list2=str1.split('>>>')
+print(list2)
+list3=str1.split('.')
+print(list3)
+list4=str1.split('',4)
+print(list4)
+print(str(list1)+'\n'+str(list2)+'\n'+str(list3)+'\n'+str(list4))
+list5=str1.split('>')
+print(list5)
