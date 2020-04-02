626f5eebe4b064bbd7201d2a3e30c30c15442574
pengziyu <13530696246@163.com>
2020-04-02 22:57:36 +0800

    字符串大小写的转换，字符串的首尾检索

---
+++ startswith方法.py
@@ -0,0 +1,3 @@
+str1='明日科技,明日奶爸,明日帝国,www.microsoft.com'
+print('判断字符串"',str1,'"是否以明日开头,结果为:',str1.startswith('明日'))  #用于检索字符串是否以指定字符串开头，是则返回True,否则返回False
+print('判断字符串"',str1,'"是否以明日开头,结果为:',str1.endswith('.com'))    #用于检索字符串是否以指定字符串结尾，是则返回True,否则返回False
---
+++ 字母的大小写转换.py
@@ -0,0 +1,5 @@
+str1='WWW.MICROSOFT.COM'
+print('原字符串',str1)
+print('新字符串',str1.lower())   #lower()将大写字母转换为小写字母
+str2='www.microsoft.com'
+print('新字符串',str2.upper())  #upper()将小写字母转化为大写字母
