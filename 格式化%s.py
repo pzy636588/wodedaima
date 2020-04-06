acd46f59714825cc99e909a9b6aac785fa6de40d
pengziyu <13530696246@163.com>
2020-04-06 23:09:43 +0800

    格式化

---
+++ 格式化%s.py
@@ -0,0 +1,6 @@
+name='娜塔莎'
+ismarry=False
+print(name)
+
+salary=8925.36
+print('我的工资是:%.1f'%salary)
---
+++ 格式化.py
@@ -0,0 +1,5 @@
+person="黄老邪"
+address='中国杭州阿里巴巴总部'
+phone='13589565246'
+print('订单收件人：'+person+'\n'+'收货地址：'+address+'\n'+'电话号码:'+phone+'\n')
+print('订单收件人：%s,收货地址:%s,电话号码:%s'%(person,address,phone))
